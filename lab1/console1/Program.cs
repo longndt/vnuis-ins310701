747d0474dfb5811ebcc8e6401ae970536222447b
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-01-23 14:48:29 +0700

    lab1

---
+++ lab1/console1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Press CTRL + F5 to run program
+            Console.WriteLine("Hello world !");
+            //Or press CTRL along with Readkey() method 
+            Console.ReadKey();
+        }
+    }
+}
