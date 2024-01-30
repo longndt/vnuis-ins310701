6928bb04e2b9e472f19c5e4a29851bfc703cc762
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-01-30 15:38:43 +0700

    lab2

---
+++ lab2/oop1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop1
+{
+    //Program.cs
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new Student objects (class instances)
+            Student s1 = new Student(12345, "Tuan Hung", "Cau Giay");
+            Student s2 = new Student(88888, "Hung Lam", "Nam Tu Liem");
+
+            //display name of student s1
+            Console.WriteLine("Name of student s1: " + s1.getName());
+
+            //display current address of student s1
+            Console.WriteLine("Current address of student s1: " + s1.getAddress());
+
+            //change address of student s1
+            s1.setAddress("Ha Dong");
+
+            //display new address of student s1
+            Console.WriteLine("New address of student s1: " + s1.getAddress());
+
+            //display all information of student s2
+            s2.displayInfo();
+        }
+    }
+}
---
+++ lab2/oop1/Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop1
+{
+    //OOP (Object Oriented Programming) class
+    //Student.cs
+    internal class Student
+    {
+        //1. attributes (variables)
+        //Note: never set access modifier
+        //of the variables to be "public"
+        //it must be "private"
+        //=> rule violation for "information hiding"
+        private int Id;
+        private string Name;
+        private string Address;
+       
+
+        //2. behaviours (methods)
+        //2.1 constructor
+        public Student() { }  //empty constructor
+        public Student(int Id, string Name, string Address)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Address = Address;
+            //=> We must use "this" keyword in this case to distinguise between left and right values.
+            //They have similar name. Left is variable and Right is parameter
+        }
+        //public Student (int i, string n, string a)
+        //{
+        //    Id = i;
+        //    Name = n;
+        //    Address = a;
+        //    => We do not need to use "this" keyword because left and right values are different name.
+        //}
+ 
+        //2.2 getter (observer)
+        public int getId()
+        {
+            return Id;
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+
+        public string getAddress()
+        {
+            return Address;
+        }
+
+        //2.3 setter (mutator)
+        public void setAddress (string Address)
+        {
+            this.Address = Address;
+        }
+
+        //2.4 extra methods
+        //2.4.1 display student information
+        public void displayInfo()
+        {
+            Console.WriteLine("Id: " + this.Id);  //this: optional in this case
+            Console.WriteLine("Name: " + this.Name);
+            Console.WriteLine("Address: " + this.Address);
+        }
+    }
+}
