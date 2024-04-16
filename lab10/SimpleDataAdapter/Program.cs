5ff9bade540d192b0792feb8a9e5cd0c33a0cb63
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-04-16 14:12:05 +0700

    lab10

--- lab10/SimpleDataAdapter/Program.cs
+++ lab10/SimpleDataAdapter/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleDataAdapter
 {
@@ -14,6 +10,8 @@ namespace SimpleDataAdapter
     {
         public static void PrintDataSet(DataSet ds)
         {
+            //Set DataSet name
+            ds.DataSetName = "AutoLot DataSet";
             Console.WriteLine($"DataSet Name: {ds.DataSetName}");
             foreach (DictionaryEntry item in ds.ExtendedProperties)
             {
@@ -38,14 +36,20 @@ namespace SimpleDataAdapter
                 }
             }
         }
-        static void Main(string[] args)
+
+        static void Main(String[] args)
         {
-            string conStr = $"Server=LG-GRAM;Database=AutoLot;Trusted_Connection=True;";
+            //Declare database connection string 
+            string db_connection = $"Server=LG-GRAM;Database=AutoLot;Trusted_Connection=True;";
+            //Declare new DataSet
             DataSet ds = new DataSet();
-            DbDataAdapter adapter = new SqlDataAdapter("select * from Inventory", conStr);
+            //Declare SQL query
+            string sql_query = "SELECT * FROM Inventory";
+            //Declare new DataAdapter
+            DbDataAdapter adapter = new SqlDataAdapter(sql_query, db_connection);
 
             //Mapping Database Names to Friendly Names
-            adapter.TableMappings.Add("Inventory", "Current Inventory");
+            adapter.TableMappings.Add("Inventory", "Demo Inventory");
             DataTableMapping tablemapping = adapter.TableMappings["Inventory"];
             tablemapping.ColumnMappings.Add("CarId", "Car Id");
             tablemapping.ColumnMappings.Add("PetName", "Car Name");
@@ -53,53 +57,6 @@ namespace SimpleDataAdapter
             //Print DataSet
             adapter.Fill(ds, "Inventory");
             PrintDataSet(ds);
-            Console.ReadLine();
-
-            //LINQ over DataTable (1)
-            //adapter.Fill(ds, "Inventory");
-            //DataTable tbl = ds.Tables[0];
-            //var subset = from car in tbl.AsEnumerable()
-            //             where ((string)car["Color"]).Equals("Black")
-            //             select car;
-            //foreach (var item in subset)
-            //{
-            //    Console.WriteLine($"{item["PetName"]} is {item["Color"]}");
-            //}
-            //Console.ReadLine();
-
-            //LINQ over DataTable (2)
-            //adapter.Fill(ds, "Inventory");
-            //DataTable tbl = ds.Tables[0];
-            //var cars = from car in tbl.AsEnumerable()
-            //           where car.Field<string>("Color") == "Black"
-            //           select new
-            //           {
-            //               ID = car.Field<int>("CarID"),
-            //               Make = car.Field<string>("Make"),
-            //               Color = car.Field<string>("Color")
-            //           };
-            //foreach (var item in cars)
-            //{
-            //    Console.WriteLine($"Car {item.ID} is a {item.Color} {item.Make}");
-            //}
-            //Console.ReadLine();
-
-            //Hydrating New DataTables from LINQ Queries
-            //adapter.Fill(ds, "Inventory");
-            //DataTable tbl = ds.Tables[0];
-            //var cars = from car in tbl.AsEnumerable()
-            //           where car.Field<string>("Color") == "Black"
-            //           select car;
-            //DataTable newTbl = cars.CopyToDataTable();
-            //foreach (DataRow row in newTbl.Rows)
-            //{
-            //    foreach (DataColumn col in newTbl.Columns)
-            //    {
-            //        Console.Write($"{row[col]}\t");
-            //    }
-            //    Console.WriteLine();
-            //}
-            //Console.ReadLine();
         }
     }
 }
