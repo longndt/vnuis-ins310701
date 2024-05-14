17bac132d8bb664db4cc8242a0aef3c8cc3dd066
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-05-14 14:31:09 +0700

    lab13

---
+++ lab13/lab13/Migrations/202405140710549_SecondMigration.cs
@@ -0,0 +1,20 @@
+﻿namespace lab13.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SecondMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "ProductImage", c => c.String());
+            AlterColumn("dbo.Products", "ProductName", c => c.String(maxLength: 30));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Products", "ProductName", c => c.String());
+            DropColumn("dbo.Products", "ProductImage");
+        }
+    }
+}
---
+++ lab13/lab13/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lab13.Models
+{
+    public class Product
+    {
+        //Primary Key
+        public int ProductId { get; set; }
+
+        //validation
+        [MinLength(5)]
+        [MaxLength(30)]
+        public string ProductName { get; set; }
+
+        //validation
+        [Range(100,9999)]
+        public double ProductPrice { get; set; }
+
+        public string ProductImage { get; set; }
+
+        //Foreign key
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
